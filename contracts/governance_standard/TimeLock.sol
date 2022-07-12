ea9d6522ad200d54ca53f5254562cf37daa6db13
rithesh6 <rithesh.kunchakuri2020@vitstudent.ac.in>
2022-07-12 16:32:05 +0530

    Updated TimeLock

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -1,5 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+  // minDelay is how long you have to wait before executing
+  // proposers is the list of addresses that can propose
+  // executors is the list of addresses that can execute
+  constructor(
+    uint256 minDelay,
+    address[] memory proposers,
+    address[] memory executors
+  ) TimelockController(minDelay, proposers, executors) {}
+}
 
 // This is an additional contract that is going to be the owner of the Box contract
 // WE want to wait for a new voke to be "executed"
 // Give time to users to get out if they dont like a governance
+
