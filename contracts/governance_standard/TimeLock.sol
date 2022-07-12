608d4bbeaa277215cc3ca8d3669f3c265696811a
rithesh6 <rithesh.kunchakuri2020@vitstudent.ac.in>
2022-07-12 16:17:48 +0530

    Updated GoverenanceContract

---
+++ contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,5 @@
+
+
+// This is an additional contract that is going to be the owner of the Box contract
+// WE want to wait for a new voke to be "executed"
+// Give time to users to get out if they dont like a governance
